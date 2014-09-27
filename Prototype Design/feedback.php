0afc69e7925e97059876f6b51cada27855a1d7c8
Samuel Yuan <yuans@rpi.edu>
2014-09-27 15:22:53 -0400

    Fixed feedback form

--- Prototype Design/feedback.php
+++ Prototype Design/feedback.php
@@ -1,6 +1,5 @@
 <?php
     $feedback=$_POST['feedback'];
-    $feedback
-    mail("rclubsme@gmail.com","Rclubs: Feedback",$feedback,);
+    mail("rclubsme@gmail.com","Rclubs: Feedback",$feedback);
     echo "Thank you for the feedback";
 ?>
