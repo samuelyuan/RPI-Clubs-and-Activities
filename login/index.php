6e7384d6120b2bba96c04eaed99859ec3cdd19c1
Samuel Yuan <yuans@rpi.edu>
2014-10-10 13:43:39 -0400

    Fixed the search bar on the other pages

--- login/index.php
+++ login/index.php
@@ -8,6 +8,7 @@
 <html>
     <head>
         <link type="text/css" rel="stylesheet" href="../css/main.css"/>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
         <title>rClubs</title>
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
