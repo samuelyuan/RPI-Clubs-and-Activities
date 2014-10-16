ed7a00d50afc150b3031b95424c903e7c7611d08
Samuel Yuan <yuans@rpi.edu>
2014-10-16 11:53:51 -0400

    Redesigned the About page

--- About/index.php
+++ About/index.php
@@ -50,7 +50,27 @@
     				{echo("Logout");}
     		?> 
         </div></a>
-
-<p>The objective of rClubs is to organize important information about all ongoing clubs and activities at RPI to make it more accessible to students. Students can create accounts and follow clubs that they wish to be a part of and they will receive important information such as the dates of key meetings from the club leaders. Overall, we hope to create a more efficient database of all the wonderful clubs here at RPI!   </p>
+        <div class="logo">
+                <center>
+                    <img src="../images/rClubs.png" width="20%;">
+                </center>
+            </div>
+        <center>
+            <p class="mission_statement">
+                rClubs is focused on making RPI's club information accessible to all students
+            </p>
+            <br/><br/>
+        </center>
+        
+        <p class="about_text">
+            <b>Never miss a club meeting again</b><br/>
+            We want you to be able to stay up to date with all of your clubs. For every club that you are part of, rClubs will notify you about important meeting times and events. 
+        </p>
+        
+        <p class="about_text">
+            <b>Can't find your club?</b><br/>
+            Feel free to notify us in the feedback form. After all, this site is currently a work in progress.  
+        </p>
+      
     </body>
 </html>
